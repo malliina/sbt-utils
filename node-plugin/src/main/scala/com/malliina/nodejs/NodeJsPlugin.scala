0d2db77ef2b30271149e0b18ebd02de4dfc8e5e2
Michael Skogberg <michael.skogberg@futurice.com>
2019-07-06 13:53:19 +0300

    Disable auto-nodecheck by default

--- node-plugin/src/main/scala/com/malliina/nodejs/NodeJsPlugin.scala
+++ node-plugin/src/main/scala/com/malliina/nodejs/NodeJsPlugin.scala
@@ -9,6 +9,7 @@ import scala.sys.process.{Process, ProcessLogger}
 
 object NodeJsPlugin extends AutoPlugin {
   object autoImport {
+    val checkNodeOnStartup = settingKey[Boolean]("When true, runs node version check on startup")
     val checkNode = taskKey[Unit]("Make sure the user uses the correct version of node.js")
     val failMode =
       settingKey[FailMode]("Whether to warn or fail hard when the node version is unsupported")
@@ -19,9 +20,11 @@ object NodeJsPlugin extends AutoPlugin {
 
   override val globalSettings: Seq[Def.Setting[_]] = Seq(
     failMode := FailMode.Warn,
+    checkNodeOnStartup := false,
     checkNode := runNodeCheck(streams.value.log, failMode.value),
     onLoad in Global := (onLoad in Global).value andThen { state =>
-      "checkNode" :: state
+      if (checkNodeOnStartup.value) "checkNode" :: state
+      else state
     }
   )
 
