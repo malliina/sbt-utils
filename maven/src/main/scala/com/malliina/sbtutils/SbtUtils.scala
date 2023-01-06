d71da6bdfac3e332666072fadd7d0b6bf579975b
Michael Skogberg <michael.skogberg@futurice.com>
2023-01-06 21:44:15 +0200

    Deps

--- maven/src/main/scala/com/malliina/sbtutils/SbtUtils.scala
+++ maven/src/main/scala/com/malliina/sbtutils/SbtUtils.scala
@@ -5,11 +5,7 @@ import sbt._
 object SbtUtils extends SbtUtils
 
 trait SbtUtils {
-  val logbackModules = Seq("classic", "core")
-
-  def loggingDeps = logbackModules.map { m =>
-    "ch.qos.logback" % s"logback-$m" % "1.2.6"
-  } ++ Seq(
-    "org.slf4j" % "slf4j-api" % "1.7.32"
-  )
+  def loggingDeps = Seq("classic", "core").map { m =>
+    "ch.qos.logback" % s"logback-$m" % "1.4.5"
+  }
 }
