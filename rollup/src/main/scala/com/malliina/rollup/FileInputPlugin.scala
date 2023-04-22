4f5c05b09ce8f396dc232faf01fae5dbb0e577d7
Michael Skogberg <michael.skogberg@futurice.com>
2023-04-22 19:37:44 +0300

    Add dep project

--- rollup/src/main/scala/com/malliina/rollup/FileInputPlugin.scala
+++ rollup/src/main/scala/com/malliina/rollup/FileInputPlugin.scala
@@ -6,9 +6,21 @@ import sbt.nio.Keys.fileInputs
 
 object FileInputPlugin extends AutoPlugin {
   val start = CommonKeys.start
+  object autoImport {
+    val dependentModule = settingKey[Project]("Module depended on")
+  }
+  import autoImport.dependentModule
+
   override def projectSettings: Seq[Def.Setting[?]] = Seq(
     start / fileInputs ++=
       (Compile / sourceDirectories).value.map(_.toGlob / ** / "*.scala") ++
-        (Compile / resourceDirectories).value.map(_.toGlob / **)
+        (Compile / resourceDirectories).value.map(_.toGlob / **),
+    start / fileInputs ++= Def
+      .settingDyn(dependentModule.value / Compile / sourceDirectories)
+      .value
+      .map(_.toGlob / ** / "*.scala") ++ Def
+      .settingDyn(dependentModule.value / Compile / resourceDirectories)
+      .value
+      .map(_.toGlob / **)
   )
 }
